ef381581754831ba9b1035664530a304d4e3d932
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-10-16 10:55:26 +0800

    finished weight init methods

--- needle/init.py
+++ needle/init.py
@@ -1,14 +1,16 @@
+# %% Import libs
+
 import math
 import needle as ndl
 
-
+# %% Helper Functions
 
 def rand(*shape, low=0.0, high=1.0, device=None, dtype="float32", requires_grad=False):
     """ Generate random numbers uniform between low and high """
     device = ndl.cpu() if device is None else device
     array = device.rand(*shape) * (high - low) + low
     return ndl.Tensor(array, device=device, dtype=dtype, requires_grad=requires_grad)
-    
+
 
 def randn(*shape, mean=0.0, std=1.0, device=None, dtype="float32", requires_grad=False):
     """ Generate random normal with specified mean and std deviation """
@@ -46,28 +48,37 @@ def one_hot(n, i, device=None, dtype="float32", requires_grad=False):
     device = ndl.cpu() if device is None else device
     return ndl.Tensor(device.one_hot(n,i.numpy(), dtype=dtype), device=device, requires_grad=requires_grad)
 
+# %% Weight Initialization
+# In all cases, the functions should return fan_in by fan_out 2D tensors
+# (extensions to other sizes can be done via e.g., reshaping).
 
 def xavier_uniform(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    a = gain * math.sqrt(6.0 / (fan_in + fan_out))
+    return rand(fan_in, fan_out, low=-a, high=a, **kwargs)
     ### END YOUR SOLUTION
 
 
 def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    std = gain * math.sqrt(2.0 / (fan_in + fan_out))
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)  # gain value for ReLU
+    bound = gain * math.sqrt(3.0 / fan_in)
+    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
     ### END YOUR SOLUTION
 
 
 def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
     ### BEGIN YOUR SOLUTION
-    raise NotImplementedError()
+    gain = math.sqrt(2.0)  # gain value for ReLU
+    std = gain / math.sqrt(fan_in)
+    return randn(fan_in, fan_out, mean=0.0, std=std, **kwargs)
     ### END YOUR SOLUTION
