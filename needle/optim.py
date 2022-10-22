9819ffc6df2d00c3ed70865eec6ad3c8bf8f3e74
Xi-Ronnie-Chen <xi.ronnie.chen@outlook.com>
2022-10-22 20:55:43 +0800

    finished sgd optmizer

--- needle/optim.py
+++ needle/optim.py
@@ -1,10 +1,13 @@
 """Optimization module"""
-import needle as ndl
-import numpy as np
 
+from typing import Dict, Iterable
+
+import needle as ndl
+
+# %% Optimizer Base Class
 
 class Optimizer:
-    def __init__(self, params):
+    def __init__(self, params: Iterable[ndl.nn.Parameter]):
         self.params = params
 
     def step(self):
@@ -14,8 +17,12 @@ class Optimizer:
         for p in self.params:
             p.grad = None
 
+# %% Stochastic Gradient Descent (SGD) Optimizer
 
 class SGD(Optimizer):
+
+    u: Dict[ndl.nn.Parameter, ndl.Tensor]
+
     def __init__(self, params, lr=0.01, momentum=0.0, weight_decay=0.0):
         super().__init__(params)
         self.lr = lr
@@ -25,9 +32,22 @@ class SGD(Optimizer):
 
     def step(self):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        for param in self.params:
+            # calculate gradients
+            param_grad = param.grad.data
+            if self.weight_decay != 0:
+                param_grad += self.weight_decay * param.data
+            # update momentum terms
+            if param not in self.u:
+                self.u[param] = (1-self.momentum) * param_grad
+            else:
+                self.u[param] = self.momentum * self.u[param].data + (1-self.momentum) * param_grad
+            param.data -= self.lr * self.u[param]
         ### END YOUR SOLUTION
 
+# Reference: https://discuss.pytorch.org/t/how-does-sgd-weight-decay-work/33105
+
+# %%  Adaptive Moment Estimation (Adam) Optimizer
 
 class Adam(Optimizer):
     def __init__(
